9cf929b17abc3a8f9a2def55a864c3e3323abef7
Ronak Nathani <ronaknnathani@gmail.com>
2016-07-05 20:35:02 -0700

    fixed typo in run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the average_degree calculation
+# example of the run script for running the rolling_median calculation
 
-# I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
+# I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
 python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 
 
