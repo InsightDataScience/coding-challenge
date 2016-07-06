7e9ecf7e15fb0539423ce6035951cb4b23543947
David E Drummond <ddrum001@users.noreply.github.com>
2016-07-06 00:10:24 -0400

    Update run.sh to note ability to accept any major language.

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-# example of the run script for running the rolling_median calculation
+# example of the run script for running the rolling_median calculation with a python file, 
+# but could be replaced with similar files from any major language
 
 # I'll execute my programs, with the input directory venmo_input and output the files in the directory venmo_output
 python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
