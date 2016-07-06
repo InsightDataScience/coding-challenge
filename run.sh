86ebd2ae51a2b66a4c881540959f28c027366835
Ronak Nathani <ronaknnathani@gmail.com>
2016-07-05 20:32:40 -0700

    updated run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # example of the run script for running the average_degree calculation
 
 # I'll execute my programs, with the input directory tweet_input and output the files in the directory tweet_output
-python ./src/average_median.py ./tweet_input/tweets.txt ./tweet_output/output.txt
+python ./src/rolling_median.py ./venmo_input/venmo-trans.txt ./venmo_output/output.txt
 
 
 
